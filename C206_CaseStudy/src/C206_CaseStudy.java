bae9f717c7cadf82ae7f3642af96f47015dc14ff
20047536 <20047536@myrp.edu.sg>
2021-08-05 12:44:49 +0800

    edited menu and options

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,10 +1,6 @@
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		C206_CaseStudy.loginToSystem();
-		C206_CaseStudy.Menu();
 
 	}
 
@@ -12,29 +8,122 @@ public class C206_CaseStudy {
 		Helper.line(30, "=");
 		System.out.println("LOGIN TO SYSTEM");
 		Helper.line(30, "=");
-	}
-
-	public static void Menu() {
-
-		Helper.line(30, "=");
-		System.out.println("Student/Parents");
-		Helper.line(30, "=");
-
-		System.out.println("1. View all CCA");
-		System.out.println("2. View Student's CCA");
-		System.out.println("3. Delete Student's CCA");
+		System.out.println("1. Student");
+		System.out.println("2. Parent");
+		System.out.println("3. Admin");
 
 		int option = -1;
 
-		while (option != 5) {
+		while (option != 3) {
 
-			Menu();
+			loginToSystem();
+			option = Helper.readInt("Enter choice > ");
+			if (option == 1) {
+				studentMenu();
+			} else if (option == 2) {
+				parentMenu();
+			} else if (option == 3) {
+				adminMenu();
+			} else {
+				System.out.println("Invalid option!");
+			}
+
+		}
+
+	}
+
+	public static void studentMenu() {
+
+		Helper.line(30, "=");
+		System.out.println("Student Profile");
+		Helper.line(30, "=");
+
+		System.out.println("1. Register ");
+		System.out.println("2. Login");
+
+		int option = -1;
+
+		while (option != 3) {
+
+			studentMenu();
+			option = Helper.readInt("Enter choice > ");
+			if (option == 1) {
+				// write code here
+			} else if (option == 2) {
+				// write code here
+			} else {
+				System.out.println("Invalid option!");
+			}
+
+		}
+	}
+
+	public static void parentMenu() {
+
+		Helper.line(30, "=");
+		System.out.println("Parent Profile");
+		Helper.line(30, "=");
+
+		System.out.println("1. Register for Student");
+		System.out.println("2. Register for Parent");
+		System.out.println("3. Login to System");
+
+		int option = -1;
+
+		while (option != 3) {
+
+			studentMenu();
 			option = Helper.readInt("Enter choice > ");
 			if (option == 1) {
 				// write code here
 			} else if (option == 2) {
 				// write code here
 			} else if (option == 3) {
+				// write code here
+			} else {
+				System.out.println("Invalid option!");
+			}
+
+		}
+	}
+
+	public static void adminMenu() {
+
+		Helper.line(30, "=");
+		System.out.println("Admin Profile");
+		Helper.line(30, "=");
+
+		System.out.println("1. Add Student to the System");
+		System.out.println("2. View Students/Parents");
+		System.out.println("3. Delete Students/Parents");
+		System.out.println("4. Add CCA");
+		System.out.println("5. Edit CCA");
+		System.out.println("6. Add CCA Category");
+		System.out.println("7. Delete CCA Category");
+		System.out.println("8. View CCA Category");
+		// add more options if needed
+
+		int option = -1;
+
+		while (option != 3) {
+
+			adminMenu();
+			option = Helper.readInt("Enter choice > ");
+			if (option == 1) {
+				// write code here
+			} else if (option == 2) {
+				// write code here
+			} else if (option == 3) {
+				// write code here
+			} else if (option == 4) {
+				// write code here
+			} else if (option == 5) {
+				// write code here
+			} else if (option == 6) {
+				// write code here
+			} else if (option == 7) {
+				// write code here
+			} else if (option == 8) {
 				// write code here
 			} else {
 				System.out.println("Invalid option!");
