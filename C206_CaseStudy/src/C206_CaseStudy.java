8cb8eb3a5baa2b43d8998c2395656e70635f713e
gyou-kun <20024466@myrp.edu.sg>
2021-08-05 12:17:15 +0800

    cca list again

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -13,18 +13,23 @@ public class C206_CaseStudy {
 		studentList.add(Molly);
 		
 		ArrayList<CCA> ccaList = new ArrayList<CCA>();
-		CCA scouts = new CCA(01, "Scouts", "Learn survival skills!", "20", "Friday", "3pm-7pm", "Back of School", "Denzel");
-		CCA football = new CCA(02, "Football", "Do you think you are the next futbol star?", "20", "Monday", "2pm-5pm", "Football Field", "Jonathan");
+		CCA scouts = new CCA(01, "Scouts", "Learn survival skills!", 20, "Friday", "3pm-7pm", "Back of School", "Denzel");
+		CCA football = new CCA(02, "Football", "Do you think you are the next futbol star?", 20, "Monday", "2pm-5pm", "Football Field", "Jonathan");
 		ccaList.add(scouts);
 		ccaList.add(football);
 		
 		C206_CaseStudy.loginToSystem();
+		
 		String studentName = Helper.readString("Enter Student Name: "); 
 		String studentID = Helper.readString("Enter Student ID: ");
 		String classGrade = Helper.readString("Enter Student's Class Grade: ");
+		String teacherName = Helper.readString("Enter Teacher's Name: ");
 		String parentName = Helper.readString("Enter Parent's Name: ");
 		String parentEmail = Helper.readString("Enter Parent's Email: ");
 		int parentNumber = Helper.readInt("Enter Parent's Contact Number: ");
+		
+		Student newStudent = new Student(studentName, studentID, classGrade, teacherName, parentName, parentEmail, parentNumber);
+		
 		System.out.println(studentList);
 		
 		C206_CaseStudy.loginToSystem();
