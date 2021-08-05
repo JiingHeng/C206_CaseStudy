8def00c6be1d9d45d1de8b1dc6b638b3f95e4623
Tay Hao Wen <20042303@myrp.edu.com>
2021-08-05 11:34:58 +0800

    student class commit

---
+++ C206_CaseStudy/src/Student.java
@@ -0,0 +1,16 @@
+/*
+ * I declare that this code was written by me. 
+ * I will not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Tay Hao Wen
+ * Student ID: 20042303
+ * Class: E37C
+ * Date/Time created: Thursday 05-08-2021 11:31
+ */
+
+public class Student {
+	private String name;
+	private String studentID;
+
+}
