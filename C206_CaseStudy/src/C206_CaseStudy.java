c6027522c232dc01156cf6eec4b84d5c9c031f86
20013327 <20013327@myrp.edu.sg>
2021-08-05 11:47:15 +0800

    UI

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -4,6 +4,28 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		C206_CaseStudy.loginToSystem(); 
+		C206_CaseStudy.Menu();
+
+	}
+	
+	
+	public static void loginToSystem() {
+		Helper.line(30, "=");
+		System.out.println("LOGIN TO SYSTEM");
+		Helper.line(30, "=");
+	}
+	
+	public static void Menu() {
+
+		Helper.line(30, "=");
+		System.out.println("Student/Parents");
+		Helper.line(30, "=");
+
+		System.out.println("1. View all CCA");
+		System.out.println("2. View Student's CCA");
+		System.out.println("3. Delete Student's CCA");
 
 	}
 
