0c3e1625582f20f2e5730df731334eba9876478b
20013327 <20013327@myrp.edu.sg>
2021-08-05 12:01:59 +0800

    Stuff

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,11 +1,23 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Student James = new Student("James", "20012345", "P4", "Desmond Lee", "Tom", "Tom@gmail.com", 12345678);
+		Student Molly = new Student("Molly", "20012344", "P3", "Desmond Lee", "Grace", "Grace@gmail.com", 23456789);
+		ArrayList<Student> studentList  = new ArrayList<Student>();
+		C206_CaseStudy.loginToSystem();
+		String studentName = Helper.readString("Enter Student Name: "); 
+		String studentID = Helper.readString("Enter Student ID: ");
+		String classGrade = Helper.readString("Enter Student's Class Grade: ");
+		String parentName = Helper.readString("Enter Parent's Name: ");
+		String parentEmail = Helper.readString("Enter Parent's Email: ");
+		int parentNumber = Helper.readInt("Enter Parent's Contact Number: ");
+		System.out.println(studentList);
 		
-		C206_CaseStudy.loginToSystem(); 
+		
 		C206_CaseStudy.Menu();
 
 	}
